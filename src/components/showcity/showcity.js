e4e2c84f3d2a6e03a16ba39749503fa0d6e2ea04
fuck-eer <nishchaychauhan8@gmail.com>
2020-08-12 21:37:49 +0530

    conditional update

--- src/components/showcity/showcity.js
+++ src/components/showcity/showcity.js
@@ -6,6 +6,15 @@ import{faCaretUp} from '@fortawesome/free-solid-svg-icons'
 const showcity=(props)=>{
   const eleicon=<FontAwesomeIcon icon={faCaretUp}/>
     
+  let changecnf=null
+  let changerec=null
+if(props.delcnf!==0){
+changecnf=<span>{eleicon}{props.delcnf}</span>
+changerec=<span>{eleicon}{props.delrec}</span>
+
+}
+
+
     return(
 
 
@@ -14,17 +23,18 @@ const showcity=(props)=>{
         <td style={{color:"#aacdbe"}}>
         {props.name}
         </td>
-      
+     
         <td className='dispfle'>
          <span  className='dolo'>{props.confirm}</span>
-          <span className='daily'>{eleicon} {props.delcnf} </span>
+          <span className='daily'>{changecnf}</span>
            </td>
            <td className='actt'>
          {props.active}
          </td>
+      
         <td className='dispfle'>
         <span   className='dolo'>{props.recover}</span>
-         <span style={{color:'#00ff00'}} className='daily'>{eleicon} {props.delrec}</span>
+         <span style={{color:'#00ff00'}} className='daily'>{changerec}</span>
          </td>
         <td style={{textAlign:'center',color:'#f5f1dd'}}>---</td>
   </tr>
--- src/components/showstate/showstate.js
+++ src/components/showstate/showstate.js
@@ -7,13 +7,24 @@ import{faCaretUp} from '@fortawesome/free-solid-svg-icons'
 const showstate=(props)=>{
 
   const eleicon=<FontAwesomeIcon icon={faCaretUp}/>
+
+  let changecnf=null
+  let changerec=null
+if(+props.delcnf!==0){
+changecnf=<span>{eleicon}{props.delcnf}</span>
+changerec=<span>{eleicon}{props.delrec}</span>
+
+}
+
+
+
 return(
    
     <tr onClick={props.click}>
     <td style={{color:"whitesmoke"}}>{props.name}</td>
-    <td className='dispfle'><span style={{color:'#f5f1da'}} className='dolo'>{props.confirm}</span><span className='daily'>{eleicon}  {props.delcnf} </span></td>
+    <td className='dispfle'><span style={{color:'#f5f1da'}} className='dolo'>{props.confirm}</span><span className='daily'>{changecnf} </span></td>
     <td className='actt'>{props.active}</td>
-    <td className='dispfle'><span style={{color:'#f5f1da'}} className='dolo'>{props.recover}</span><span style={{color:'#00ff00'}} className='daily'> {eleicon} {props.delrec}</span></td>
+    <td className='dispfle'><span style={{color:'#f5f1da'}} className='dolo'>{props.recover}</span><span style={{color:'#00ff00'}} className='daily'> {changerec}</span></td>
     <td className='actt'>{props.update}</td>
     </tr>
 
