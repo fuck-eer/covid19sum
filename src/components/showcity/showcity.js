10d4740433d58737d31800cb8e602d811c035a89
fuck-eer <nishchaychauhan8@gmail.com>
2020-08-19 03:14:48 +0530

    number update

--- src/components/showcity/showcity.js
+++ src/components/showcity/showcity.js
@@ -1,46 +1,54 @@
 import React from 'react'
+import CountUp from 'react-countup'
 // import '../../containers/showstates/showstate.css'
 import './showcity.css'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import{faCaretUp} from '@fortawesome/free-solid-svg-icons'
-const showcity=(props)=>{
-  const eleicon=<FontAwesomeIcon icon={faCaretUp}/>
-    
-  let changecnf=null
-  let changerec=null
-if(props.delcnf!==0){
-changecnf=<span>{eleicon}{props.delcnf}</span>
-changerec=<span>{eleicon}{props.delrec}</span>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons'
+const showcity = props => {
+  const eleicon = <FontAwesomeIcon icon={faCaretUp} />
 
+  let changecnf = null
+  let changerec = null
+  if (props.delcnf !== 0) {
+    changecnf = (
+      <span>
+        {eleicon}
+        <CountUp end={props.delcnf} />
+      </span>
+    )
+    changerec = (
+      <span>
+        {eleicon}
+        <CountUp end={props.delrec} />
+      </span>
+    )
+  }
+
+  return (
+    <tr>
+      <td style={{ color: '#aacdbe' }}>{props.name}</td>
+
+      <td className='dispfle'>
+        <span className='dolo'>
+          <CountUp end={props.confirm} />
+        </span>
+        <span className='daily'>{changecnf}</span>
+      </td>
+      <td className='actt'>
+        <CountUp end={props.active} />
+      </td>
+
+      <td className='dispfle'>
+        <span className='dolo'>
+          <CountUp end={props.recover} />
+        </span>
+        <span style={{ color: '#00ff00' }} className='daily'>
+          {changerec}
+        </span>
+      </td>
+      <td style={{ textAlign: 'center', color: '#f5f1dd' }}>---</td>
+    </tr>
+  )
 }
 
-
-    return(
-
-
-
-  <tr >
-        <td style={{color:"#aacdbe"}}>
-        {props.name}
-        </td>
-     
-        <td className='dispfle'>
-         <span  className='dolo'>{props.confirm}</span>
-          <span className='daily'>{changecnf}</span>
-           </td>
-           <td className='actt'>
-         {props.active}
-         </td>
-      
-        <td className='dispfle'>
-        <span   className='dolo'>{props.recover}</span>
-         <span style={{color:'#00ff00'}} className='daily'>{changerec}</span>
-         </td>
-        <td style={{textAlign:'center',color:'#f5f1dd'}}>---</td>
-  </tr>
- 
-
-    );
-};
-
-export default showcity;
+export default showcity
--- src/components/showstate/showstate.js
+++ src/components/showstate/showstate.js
@@ -1,36 +1,52 @@
 import React from 'react'
 import './showstate.css'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import{faCaretUp} from '@fortawesome/free-solid-svg-icons'
+import CountUp from 'react-countup'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons'
 
+const showstate = props => {
+  const eleicon = <FontAwesomeIcon icon={faCaretUp} />
 
-const showstate=(props)=>{
+  let changecnf = null
+  let changerec = null
+  if (+props.delcnf !== 0) {
+    changecnf = (
+      <span>
+        {eleicon}
+        <CountUp end={props.delcnf} />
+      </span>
+    )
+    changerec = (
+      <span>
+        {eleicon}
+        <CountUp end={props.delrec} />
+      </span>
+    )
+  }
 
-  const eleicon=<FontAwesomeIcon icon={faCaretUp}/>
-
-  let changecnf=null
-  let changerec=null
-if(+props.delcnf!==0){
-changecnf=<span>{eleicon}{props.delcnf}</span>
-changerec=<span>{eleicon}{props.delrec}</span>
-
-}
-
-
-
-return(
-   
+  return (
     <tr onClick={props.click}>
-    <td style={{color:"whitesmoke"}}>{props.name}</td>
-    <td className='dispfle'><span style={{color:'#f5f1da'}} className='dolo'>{props.confirm}</span><span className='daily'>{changecnf} </span></td>
-    <td className='actt'>{props.active}</td>
-    <td className='dispfle'><span style={{color:'#f5f1da'}} className='dolo'>{props.recover}</span><span style={{color:'#00ff00'}} className='daily'> {changerec}</span></td>
-    <td className='actt'>{props.update}</td>
+      <td style={{ color: 'whitesmoke' }}>{props.name}</td>
+      <td className='dispfle'>
+        <span style={{ color: '#f5f1da' }} className='dolo'>
+          <CountUp end={props.confirm} />
+        </span>
+        <span className='daily'>{changecnf} </span>
+      </td>
+      <td className='actt'>
+        <CountUp end={props.active} />
+      </td>
+      <td className='dispfle'>
+        <span style={{ color: '#f5f1da' }} className='dolo'>
+          <CountUp end={props.recover} />
+        </span>
+        <span style={{ color: '#00ff00' }} className='daily'>
+          {changerec}
+        </span>
+      </td>
+      <td className='actt'>{props.update}</td>
     </tr>
-
-
-);
-
+  )
 }
 
-export default showstate;
+export default showstate
